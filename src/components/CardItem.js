748c7f538df5d957715ae9648923b939a2829758
Khadicha <246abdurashidova123@gmail.com>
2023-04-25 23:32:37 +0500

    Codes added to CardItem.js and Products.js

---
+++ src/components/CardItem.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const CardItem = () => {
+  return (
+    <div>
+      <div></div>
+    </div>
+  );
+};
+
+export default CardItem;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
+import CardItem from "./CardItem";
 
-const Cart = () => {
+const Cart = ({ products, changeNumb }) => {
   return (
     <div>
       <div></div>
