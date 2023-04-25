a33533b06131cb796a03fd079499111be68e9ff1
Khadicha <246abdurashidova123@gmail.com>
2023-04-25 22:14:24 +0500

    Writing code in app.js started

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
-  );
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const addToCart = (item) => {
+    const productList = [...cart];
+    if (!productList.includes(item)) {
+      productList.push(item);
+    }
+
+    const index = productList.indexOf(item);
+    productList[index].quantity++;
+    setCart(productList);
+    localStorage.setItem("cart", JSON.stringify(productList));
+
+    alert("Pizza added", cart);
+    console.log(index);
+  };
+  return <div></div>;
 }
 
 export default App;
