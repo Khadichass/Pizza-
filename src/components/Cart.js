cbeedd83c3f775d916b5db59a752a85d8ff08aa5
Khadicha <246abdurashidova123@gmail.com>
2023-04-28 23:02:21 +0500

    function added

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,6 +2,18 @@ import React, { useState, useEffect } from "react";
 import CardItem from "./CardItem";
 
 const Cart = ({ products, changeNumb }) => {
+  /* Shopping card inside  */
+
+  const [classActive, toggleClass] = useState(flase);
+  const [sum, setSum] = useState(0);
+
+  const toggleButton = () => {
+    toggleClass(!classActive);
+  };
+
+  useEffect(() => {
+    let total = 0;
+  });
   return (
     <div>
       <div></div>
