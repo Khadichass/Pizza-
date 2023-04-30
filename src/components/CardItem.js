6a0d61ce05e7b135ec695c0042d7654747a5e6eb
Khadicha <246abdurashidova123@gmail.com>
2023-04-30 22:27:58 +0500

    Cart.js finished CartItem.js started

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
+import setProducts from "./Products";
 
 const CardItem = () => {
   return (
-    <div>
-      <div></div>
+    <div className="cartItem">
+      <img src={product.url} alt="cart-item" className="cartItemImage" />
     </div>
   );
 };
