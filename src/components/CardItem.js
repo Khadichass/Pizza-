a84d7cb9b5bdadabc6aa59f5548185f65448fa2b
Khadicha <246abdurashidova123@gmail.com>
2023-04-30 22:49:22 +0500

    CartItem finished

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,10 +1,40 @@
 import React, { useState, useEffect } from "react";
 import setProducts from "./Products";
 
-const CardItem = () => {
+const CardItem = (product, changeNumb) => {
   return (
     <div className="cartItem">
       <img src={product.url} alt="cart-item" className="cartItemImage" />
+      <div className="cartMain">
+        <div>
+          <p>{product.title}</p>
+          <span>UZS:{product.price}</span>
+        </div>
+        <div>
+          <p>
+            Date <span>{product.quantity}</span>
+          </p>
+
+          <div>
+            <button
+              className="quantityBtn"
+              onClick={() => {
+                changeNumb(product, "-");
+              }}
+            >
+              -
+            </button>
+            <button
+              className="quantityBtn"
+              onClick={() => {
+                changeNumb(product, "+");
+              }}
+            >
+              +
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
