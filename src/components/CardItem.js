9c2f5cd3d9b7b93968032bde7d79dff569f64b57
Khadicha <246abdurashidova123@gmail.com>
2023-05-01 12:07:42 +0500

    new codes added

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,33 +1,71 @@
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const CardItem = (product, changeNumb) => {
+// const CardItem = (product, changeNumb) => {
+//   return (
+//     <div className="cartItem">
+//       <img src={product.url} alt="cart-item" className="cartItemImage" />
+//       <div className="cartMain">
+//         <div>
+//           <p>{product.title}</p>
+//           <span>UZS:{product.price}</span>
+//         </div>
+//         <div>
+//           <p>
+//             Date <span>{product.quantity}</span>
+//           </p>
+
+//           <div>
+//             <button
+//               className="quantityBtn"
+//               onClick={() => {
+//                 changeNumb(product, "-");
+//               }}
+//             >
+//               -
+//             </button>
+//             <button
+//               className="quantityBtn"
+//               onClick={() => {
+//                 changeNumb(product, "+");
+//               }}
+//             >
+//               +
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CardItem;
+
+import React, { useState, useEffect } from "react";
+// import Products from "./Products";
+
+const CardItem = ({ Products, changee }) => {
   return (
     <div className="cartItem">
-      <img src={product.url} alt="cart-item" className="cartItemImage" />
-      <div className="cartMain">
+      <img src={Products.url} alt="cartItem" className="cartItemimg" />
+      <div className="carMain">
         <div>
-          <p>{product.title}</p>
-          <span>UZS:{product.price}</span>
+          <p>{Products.title}</p>
+          <span>UZS{Products.price}</span>
         </div>
         <div>
           <p>
-            Date <span>{product.quantity}</span>
+            Число:<span>{Products.quantity}</span>
           </p>
-
           <div>
             <button
-              className="quantityBtn"
-              onClick={() => {
-                changeNumb(product, "-");
-              }}
+              className="quantityBTN"
+              onClick={() => changee(Products, "-")}
             >
               -
             </button>
             <button
-              className="quantityBtn"
-              onClick={() => {
-                changeNumb(product, "+");
-              }}
+              className="quantityBTN"
+              onClick={() => changee(Products, "+")}
             >
               +
             </button>
